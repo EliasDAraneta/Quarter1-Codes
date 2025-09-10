8830e1b150b5814302ffc1db00274168e42657a0
EliasDAraneta <juanelias.araneta@smc.pshs.edu.ph>
2025-09-10 08:45:53 +0800

    Update SG6_Code1.py
    sum is rounded to two decimal places

--- SG6_Code1.py
+++ SG6_Code1.py
@@ -1,5 +1,5 @@
 num1 = float(input("Enter the first number:"))
 num2 = float(input("Enter the second number:"))
 num3 = float(input("Enter the third number:"))
-sum = num1 + num2 + num3
+sum = round(num1 + num2 + num3, 2)
 print("The total of the three numbers is:", sum)
